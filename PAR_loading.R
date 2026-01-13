9f681099c207c6cb2bb713dda71d3d4512b91517
Edward Gregr <ejgregr@gmail.com>
2026-01-13 10:51:44 -0800

    Finalized PAR to DLI work

--- PAR_loading.R
+++ PAR_loading.R
@@ -1,11 +1,15 @@
 ##### Radiation loading - from DSR to PAR #####
 # Created Dec, 2025
 #
-############################################################################
 # NOTES:
-# none yet.
+# CSVs (one per month) created using Python script in GEE. CSV files contain
+# hourly estimates of downward surface solar radiation. NB the source data (ERA5)
+# has large pixels.
+# 
+# See project report for details.
+# 
 ############################################################################
-
+library(ggplot2)
 
 # Directory containing monthly CSVs
 csv_dir <- "GEE_exports"
@@ -14,8 +18,6 @@ csv_dir <- "GEE_exports"
 files <- list.files( path = paste0(source_dir, "/GEE_exports"),
                              pattern = "\\.csv$",
                              full.names = TRUE )
-
-
 era5_df <- do.call(
   rbind,
   lapply(files, function(f) {
@@ -26,65 +28,112 @@ era5_df <- do.call(
 )
 
 # Keep only required columns
+# stdDev is across the 11 pixels selected by the Broughton mask
 era5_df <- era5_df[, c("Timestamp", "mean", "stdDev")]
 
 # Ensure time ordering
 era5_df <- era5_df[order(era5_df$Timestamp), ]
-
 head(era5_df)
 
+# Convert DSR to PAR ... 
+par_df <- era5_df
+par_df$mean   <- (era5_df$mean   / 3600) * 0.5 * 4.57
+par_df$stdDev <- (era5_df$stdDev / 3600) * 0.5 * 4.57
 
-library(ggplot2)
+# Hourly plot is messy and time-consuming to produce. Plot daily if you like
+daily_PAR_plot( par_df )
 
-ggplot(era5_df, aes(x = Timestamp, y = mean)) +
-  geom_errorbar(
-    aes(
-      ymin = mean - stdDev,
-      ymax = mean + stdDev
-    ),
-    width = 0,            # vertical lines only
-    alpha = 0.5,
-    linewidth = 0.4
-  ) +
-  geom_point(
-    size = 1.2,
-    color = "black"
-  ) +
-  labs(
-    x = "Time (UTC)",
-    y = expression("Surface solar radiation downwards (J m"^-2~")"),
-    title = "ERA5 hourly surface solar radiation (mean ± SD)"
-  ) +
-  theme_bw()
+# Sum PAR to get a daily light interval .... 
+
+DLI_df <- calc_dli( par_df )
+head(DLI_df)
+
+DLI_plot( DLI_df )
 
 
-# downsample to daily ... 
 
-era5_daily <- aggregate(
-  cbind(mean, stdDev) ~ as.Date(Timestamp),
-  data = era5_df,
-  FUN = mean
-)
 
-names(era5_daily)[1] <- "Date"
 
-ggplot(era5_daily, aes(x = Date, y = mean)) +
-  geom_errorbar(
-    aes(
-      ymin = mean - stdDev,
-      ymax = mean + stdDev
-    ),
-    width = 0,            # vertical lines only
-    alpha = 0.5,
-    linewidth = 0.4
-  ) +
-  geom_point(
-    size = 1.2,
-    color = "black"
-  ) +
-  labs(
-    x = "Date",
-    y = expression("Surface solar radiation downwards (J m"^-2~")"),
-    title = "ERA5 daily surface solar radiation (mean ± SD)"
-  ) +
-  theme_bw()
+
+#------------------------- PAR support FUNCTIONS -----------------------------
+
+#---- Daily light interval from PAR ----
+calc_dli <- function(df) {
+  df$Date <- as.Date(df$Timestamp)
+  
+  # Sum the means per day and convert to mol/m2/d
+  # (Sum * 3600 / 1000000 = Sum * 0.0036)
+  daily_mean <- aggregate(mean ~ Date, data = df, FUN = function(x) sum(x) * 0.0036)
+  colnames(daily_mean)[2] <- "mean"
+  
+  # Propagate uncertainty: sqrt(sum(stdDev^2)) * 0.0036
+  daily_sd <- aggregate(stdDev ~ Date, data = df, FUN = function(x) sqrt(sum(x^2)) * 0.0036)
+  colnames(daily_sd)[2] <- "dli_stdDev"
+  
+  # Merge results into a single data frame
+  result <- merge(daily_mean, daily_sd, by = "Date")
+  
+  return(result)
+}
+
+#----- Plot DLI   ----
+DLI_plot <-function( dli_dat ){
+  
+  ggplot(dli_dat, aes(x = Date, y = mean)) +
+    geom_errorbar(
+      aes(
+        ymin = mean - stdDev,
+        ymax = mean + stdDev
+      ),
+      width = 0,            # vertical lines only
+      alpha = 0.5,
+      linewidth = 0.4
+    ) +
+    geom_point(
+      size = 1.2,
+      color = "black"
+    ) +
+    labs(
+      x = "Date",
+      y = expression("Daily Light Interval (mol m"^-2~" d"^-1~")"),
+      title = "Daily Light Interval (mean ± SD)"
+    ) +
+    theme_bw()
+}
+
+#----- Plot daily PAR data  ----
+daily_PAR_plot <-function( par_dat ){
+  
+  # downsample to daily ... 
+  par_daily <- aggregate(
+    cbind(mean, stdDev) ~ as.Date(Timestamp),
+    data = par_dat,
+    FUN = mean
+  )
+  
+  names(par_daily)[1] <- "Date"
+  
+  ggplot(par_daily, aes(x = Date, y = mean)) +
+    geom_errorbar(
+      aes(
+        ymin = mean - stdDev,
+        ymax = mean + stdDev
+      ),
+      width = 0,            # vertical lines only
+      alpha = 0.5,
+      linewidth = 0.4
+    ) +
+    geom_point(
+      size = 1.2,
+      color = "black"
+    ) +
+    labs(
+      x = "Date",
+      y = expression("Photosynthetically Active Radiation (umol m"^-2~" s"^-1~")"),
+      title = "Daily Photosynthetically Active Radiation (mean ± SD)"
+    ) +
+    theme_bw()
+}
+
+#----
+# FIN.
